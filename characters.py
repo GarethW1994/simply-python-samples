e7b34b32448e29f5d5000477d1a6ba30fe461b81
Gareth <williams.gareth0@gmail.com>
2018-03-01 15:17:32 +0200

    added some sentence manipulation

---
+++ characters.py
@@ -0,0 +1,26 @@
+### simple sentence
+sentence = "Gareth was playing basketball"
+
+## Get the first character of the sentence
+firstChar = sentence[0]
+
+## print first char
+print(firstChar)
+
+## Get the fifth character of the sentence
+fifthChar = sentence[4]
+
+## print fifth char
+print(fifthChar)
+
+## Get the the 'Gareth' from the sentence
+name = sentence[0:6]
+
+## print the name
+print(name)
+
+## Get the first three characters of the sentence
+firstThree = sentence[:3]
+
+## print first three characters
+print(firstThree)
