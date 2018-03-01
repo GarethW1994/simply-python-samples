5191570599fc15be0cf7de46657340e929e61093
Gareth <williams.gareth0@gmail.com>
2018-03-01 15:05:42 +0200

    added some arithmetic operators

---
+++ arithmetic.py
@@ -0,0 +1,14 @@
+age1 = 12
+age2 = 18
+
+addition = age1 + age2
+multiply = age1 * age2
+subtract = age2 - age1
+divide = age2 / age1
+mod = age2 % age1
+
+print(addition)
+print(multiply)
+print(subtract)
+print(divide)
+print(mod)
